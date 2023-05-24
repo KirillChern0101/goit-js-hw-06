650d89abc60bac161e20e0cd6dc45b69ea2b1924
KirillChern <kirillchernliko@gmail.com>
2023-05-24 22:17:17 +0300

    +1, 2 hw

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,41 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// const list = document.querySelector("#ingredients");
+// const liArray = [];
+
+// ingredients.forEach((ingredient) => {
+//   const item = document.createElement("li");
+//   item.className = "item";
+//   item.textContent = ingredient;
+//   liArray.push(item);
+// });
+
+// list.append(...liArray);
+
+// const ingredientList = document.createElement("li");
+// ingredientList.textContent = "Tomatos";
+// ingredientList.classList.add("item");
+// console.log(ingredientList);
+
+// const ingredientsEl = document.querySelector("#ingredients");
+// ingredientsEl.appendChild(ingredientList);
+// console.log(ingredientsEl);
+
+const ingredientList = document.querySelector("#ingredients");
+const ingredientArray = [];
+
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  ingredientArray.push(item);
+});
+
+ingredientList.append(...ingredientArray);
