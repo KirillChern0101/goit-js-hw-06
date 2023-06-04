f3edb3d4eaee8e724cec13feaf1fb688e363f4d4
KirillChern <kirillchernliko@gmail.com>
2023-06-04 17:28:39 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,16 +3,13 @@ console.log(validationInput.getAttribute("data-length"));
 
 validationInput.addEventListener("blur", (event) => {
   if (
-    event.target.value.length === validationInput.getAttribute("data-length")
+    event.target.value.length ===
+    parseInt(validationInput.getAttribute("data-length"))
   ) {
     validationInput.classList.add("valid");
-    if (validationInput.classList.contains("invalid")) {
-      validationInput.classList.remove("invalid");
-    }
+    validationInput.classList.remove("invalid");
   } else {
-    if (validationInput.classList.contains("valid")) {
-      validationInput.classList.remove("valid");
-    }
     validationInput.classList.add("invalid");
+    validationInput.classList.remove("valid");
   }
 });
